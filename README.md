82cb70ff1162063fa7f2c49407db2eb1aaca3934
austin-darrow <adarrow@utexas.edu>
2022-07-18 09:55:06 -0500

    Update game with simple ui

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rock-paper-scissors
 
-\\Build a console program that plays Rock, Paper, Scissors against the user. Written in JavaScript.
+//Build a console program that plays Rock, Paper, Scissors against the user. Written in JavaScript.
+
+//Build a UI for the game so that players don't have to use the console anymore.
--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-//Prompt user to choose rock, paper, or scissors. Store choice in variable. Make sure it's case insensitive
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+let play = document.querySelector('#play');
+let scoreboard = document.querySelector('#score');
 
-let userPlay = () => prompt("Choose one: Rock, Paper, Scissors").toLowerCase();
+let userPlay = (play) => {
+    return play;
+};
 
-//Use random method for computer to choose rock, paper, or scissors. Store in separate variable
+rock.addEventListener('click', () => {match(userPlay('rock'), computerPlay())});
+paper.addEventListener('click', () => {match(userPlay('paper'), computerPlay())});
+scissors.addEventListener('click', () => {match(userPlay('scissors'), computerPlay())});
 
 let computerPlay = function() {
     let rand = Math.floor((Math.random() * 3) + 1); //return number between 1 and 3
@@ -13,56 +21,33 @@ let computerPlay = function() {
     } else {
         return "scissors";
     }
-}
-
-//Compare user vs. computer, console.log winner of that round
+};
 
 let userScore = 0;
 let computerScore = 0;
-let score = () => console.log(`You've won ${userScore} rounds. Computer has won ${computerScore} rounds.`);
+let userScoreDisplay = document.querySelector('#userScoreDisplay');
+let computerScoreDisplay = document.querySelector('#computerScoreDisplay');
 
-let match = function(userPlay, computerPlay) {
+
+let match = (userPlay, computerPlay) => {
+    if (userScore == 5) {
+        play.textContent = `You win the game! Refresh to play again.`;
+    } else if (computerScore == 5) {
+        play.textContent = `The computer wins the game. Refresh to play again.`;
+    } else {
     switch (true) {
         case userPlay == computerPlay:
-            console.log(`you both played ${userPlay}! It's a tie!`);
+            play.textContent = `you both played ${userPlay}! It's a tie!`;
             break;
         case (userPlay == "rock" && computerPlay == "scissors") || (userPlay == "paper" && computerPlay == "rock") || (userPlay == "scissors" && computerPlay == "paper"):
-            console.log(`You played ${userPlay}. The computer played ${computerPlay}. You win this round!`);
-            return userScore++;
+            play.textContent = `You played ${userPlay}. The computer played ${computerPlay}. You win this round!`;
+            userScore++;
+            userScoreDisplay.textContent = `Your Score: ${userScore}`;
+            return userScore;
         default:
-            console.log(`You played ${userPlay}. The computer played ${computerPlay}. The computer wins this round!`);
-            return computerScore++;
-    }
-}
-
-function playGame() {
-    while (userScore < 3 && computerScore < 3) {
-        match(userPlay(),computerPlay());
-        console.log(`User Score = ${userScore}`);
-        console.log(`Computer Score = ${computerScore}`);
-        score();
-    }
-    if (userScore == 3) {
-        let repeat = confirm("You win the game! Congrats! Play again?");
-        if (repeat) {
-            userScore = 0;
-            computerScore = 0;
-            playGame();
-        } else {
-            console.log("Goodbye!")
-        }
-    } else {
-        let repeat = confirm("You lose the game. Try again?");
-        if (repeat) {
-            userScore = 0;
-            computerScore = 0;
-            playGame();
-        } else {
-            console.log("Goodbye!")
-        }
-    }
-}
-
-playGame();
-
-//Continue playing until either user or computer wins 3 rounds, then console.log overall winner
+            play.textContent = `You played ${userPlay}. The computer played ${computerPlay}. The computer wins this round!`;
+            computerScore++;
+            computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+            return computerScore;
+    }};
+};
